c51dcddc940ae648eefbabe599eaa879bb49c7d0
dlgustn3768 <dlgustn3768@naver.com>
2023-04-10 16:34:47 +0900

    StudentRespDto 생성, 학생전체 조회 및 ID 조회 추가

--- src/main/java/com/web/study/domain/entity/Student.java
+++ src/main/java/com/web/study/domain/entity/Student.java
@@ -2,6 +2,8 @@ package com.web.study.domain.entity;
 
 import java.time.LocalDate;
 
+import com.web.study.dto.response.StudentRespDto;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,4 +14,12 @@ public class Student {
 	private int id;
 	private String name;
 	private LocalDate birth_date;
+	
+	public StudentRespDto toDto() {
+		return StudentRespDto.builder()
+				.id(id)
+				.name(name)
+				.birthDate(birth_date)
+				.build();
+	}
 }
