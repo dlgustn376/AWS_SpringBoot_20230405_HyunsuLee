64cb90e2459c8364dfb17d4d4de57bf24c0b418d
dlgustn3768 <dlgustn3768@naver.com>
2023-04-10 16:05:04 +0900

    response Dto를 생성해서 스네이크 표기를 카멜표기로 바꾸는 toDto 메서드 사용

--- src/main/java/com/web/study/service/LecturerServiceImpl.java
+++ src/main/java/com/web/study/service/LecturerServiceImpl.java
@@ -1,8 +1,12 @@
 package com.web.study.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.web.study.dto.request.lecturer.LecturerReqDto;
+import com.web.study.dto.response.LecturerRespDto;
 import com.web.study.repository.LecturerRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -16,6 +20,21 @@ public class LecturerServiceImpl implements LecturerService {
 	@Override
 	public void registeLecturer(LecturerReqDto lecturerReqDto) {
 		lecturerRepository.saveLecturer(lecturerReqDto.toEntity());
+	}
+
+	@Override
+	public List<LecturerRespDto> getLectureAll() {
+		List<LecturerRespDto> dtos = new ArrayList<>();
+		
+		lecturerRepository.getLecturerAll().forEach(entity -> {
+			dtos.add(entity.toDto());
+		});
+		return dtos;
+	}
+
+	@Override
+	public LecturerRespDto findLecturerById(int id) {
+		return lecturerRepository.findLecturerById(id).toDto();
 	}
 	
 }
