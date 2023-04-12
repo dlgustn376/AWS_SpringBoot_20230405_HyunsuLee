ab7f16da780df75db8b0b54f3442f8ffc6960fb4
dlgustn3768 <dlgustn3768@naver.com>
2023-04-12 17:09:30 +0900

    Advice init, ValidAspect init, SearchCourseReqDto init

--- src/main/java/com/web/study/controller/lecture/CourseController.java
+++ src/main/java/com/web/study/controller/lecture/CourseController.java
@@ -1,6 +1,9 @@
-package com.web.study.controller.lecture;
+ package com.web.study.controller.lecture;
+
+import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.web.study.aop.annotation.CheckNameAspect;
 import com.web.study.aop.annotation.ParamsAspect;
 import com.web.study.aop.annotation.TimerAspect;
+import com.web.study.aop.annotation.ValidAspect;
 import com.web.study.dto.DataResponseDto;
 import com.web.study.dto.ResponseDto;
 import com.web.study.dto.request.course.CourseReqDto;
+import com.web.study.dto.request.course.SearchCourseReqDto;
 import com.web.study.service.CourseService;
 
 import lombok.RequiredArgsConstructor;
@@ -34,14 +39,27 @@ public class CourseController {
 	@TimerAspect
 	@GetMapping("/course/all")
 		public ResponseEntity<? extends ResponseDto> getCourseAll(){
-		
+//		if(1 == 1) {
+//			throw new CustomException("예외 만들기");
+//		}
 		return ResponseEntity.ok().body(DataResponseDto.of(courseService.getCourseAll()));
 	}
-	
+	@ValidAspect
 	@ParamsAspect
-	@GetMapping("/search/courses")
-	public ResponseEntity<? extends ResponseDto> searchCourse(int type,String searchValue){  //@RequestParam 생략된것
-		return ResponseEntity.ok().body(DataResponseDto.of(courseService.searchCourse(type, searchValue)));
+	@GetMapping("/search/courses")								// @Valid 와 BindingResult는 세트
+	public ResponseEntity<? extends ResponseDto> searchCourse(@Valid SearchCourseReqDto searchCourseReqDto, BindingResult bindingResult){  //@RequestParam 생략된것
+
+//		if(bindingResult.hasErrors()) {
+//			Map<String,String> errorMap = new HashMap<>();
+//			
+//			bindingResult.getFieldErrors().forEach(error -> {
+//				errorMap.put(error.getField(), error.getDefaultMessage());
+//			});
+//			
+//			throw new CustomException("유효성 검사 실패",errorMap);
+//		}
+		
+		return ResponseEntity.ok().body(DataResponseDto.of(courseService.searchCourse(searchCourseReqDto.getType(), searchCourseReqDto.getSearchValue())));
 	}
 	
 	
---
+++ src/main/java/com/web/study/exception/CustomException.java
@@ -0,0 +1,21 @@
+package com.web.study.exception;
+
+import java.util.Map;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException{
+	private static final long serialVersionUID = -4631732031026685557L;
+	
+	private Map<String, String> errorMap;
+	
+	public CustomException(String message) {
+		super(message);
+	}
+	
+	public CustomException(String message, Map<String, String> errorMap) {
+		super(message);
+		this.errorMap = errorMap;
+	}
+}
